57b09c6fdf160c03e6ee33b75557aae23c120503
Andrey Jesús Morales Navarro <304560419@uca.ac.cr>
2025-11-20 21:26:13 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gestión_de_Reservas___Laboratorios_de_Cómputo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
