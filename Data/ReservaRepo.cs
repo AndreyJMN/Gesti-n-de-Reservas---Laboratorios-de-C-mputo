f81eeca6f08d84fa7e7d983a80063596967ac088
Andrey Jesús Morales Navarro <304560419@uca.ac.cr>
2025-11-20 23:09:47 -0600

    Se crea carpeta Reserva.Repor donde se guardan las reservas.

--- Data/ReservaRepo.cs
+++ Data/ReservaRepo.cs
@@ -1,6 +1,16 @@
-﻿namespace Gestión_de_Reservas___Laboratorios_de_Cómputo.Data
+﻿using Gestión_de_Reservas___Laboratorios_de_Cómputo.Models;
+
+namespace Gestión_de_Reservas___Laboratorios_de_Cómputo.Data
 {
     public class ReservaRepo
     {
+        private static readonly List<Reserva> _reservas = new():
+            private static int _nextId = 1;
+        public static IReadOnlyList<Reserva> ObtenerTodos() => _reservas.AsReadOnly();
+        public static void Agregar(Reserva p)
+        {
+            p.Id = _nextId++;
+            _reservas.Add(p);
+        }
     }
 }
